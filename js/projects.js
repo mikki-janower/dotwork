93745fd4ded4a307b6acbb8b83c56435a3486817
Mikki Janower <65423058+mikki-janower@users.noreply.github.com>
2023-09-22 17:36:11 -0400

    REFRESH!!!!!

--- js/projects.js
+++ js/projects.js
@@ -16,12 +16,10 @@ observer.observe();
 //--------------------start at the top of a page after every page redirect
 window.scrollTo(0, 0);
 
-//------------standardize the site title on every page across the site. 
-$(".name").append("mikki.studio");
-
-//---------make the 'about' section of the index slide in after about half a second
-$('#about').delay(300).slideToggle(300);
-
+//---------append nav to the #navcontainer section on every page
+$('#navcontainer').append('<div class="name"><h2><a href="https://mikki.studio">Mikki Janower</a></h2></div><div class="nav-1"><h2><a href="about.html">About</a><a href="mailto:info@mikki.studio" target="_blank">Contact</a></h2></div><div class="nav-2"><h2><a href="https://www.instagram.com/_miikki/" target="_blank">Instagram</a><a href="https://www.are.na/mikki-janower" target="_blank">Are.na</a></h2></div>');
+//---------append footer to the #projfooter section on every page
+$('#projfooter').append('<h2><a class="btn-back flip">Back</a></h2><h2><a class="btn-next flip">Next</a></h2>');
 //----------------When you hover on a listing on the index, fade out its label.
    $(".listing").each(function(i){
     $(this).mouseover(function(){
@@ -37,13 +35,10 @@ $('#about').delay(300).slideToggle(300);
         window.location.replace($(this).data("link"));
     });
 
-//--------------------load project case studies with a subtle fade effect
-$("#project").delay("200").fadeIn("300");
-
-
 //this array lists all currently active case studies in order. The 'back' and 'next' functions below navigate between its contents.
 //manually update this array every time you'd like to add a new case study or change the order of the existing ones. 
 const pagelinks = [
+    "fossora,html",
     "mhns.html",
     "community.html",
     "leren.html",
@@ -52,8 +47,6 @@ const pagelinks = [
     "transientgrounds.html",
     "cyberotica.html",
     "joshualeifer.html",
-    "mariovoid.html",
-    "sober21.html",
     "reconnatypeface.html",
     "reflections.html",
     "postcursor.html"
@@ -159,9 +152,6 @@ $('.btn-next').click(function(){
             next();
         });
     });
-
-    //------------------------Insert footer menu onto every project page
-    $(".bottom").css({"position": "fixed"}).append("<a class='flip' href='https://www.instagram.com/_miikki/' target='_blank'>Instagram</a><a class='flip' href='mailto:info@mikki.studio' target='_blank'>Email</a><a class='flip' href='https://www.are.na/mikki-janower' target='_blank'>Are.na</a>");
 
     //------insert all scripts and into the <head> tag of every page on the site instead of repeating it on each (this lets me batch edit)
     $("head").append('<meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">');
